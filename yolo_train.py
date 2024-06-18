de3bf3c4d4b0e4e0a333c1facb3547f3c2548c7e
Rui Cao <caorui0303@gmail.com>
2024-06-18 11:26:25 +0800

    remove yolo script

--- yolo_train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
-# Train the model with 2 GPUs
-results = model.train(data="uoais_sim.yaml", epochs=24, imgsz=640, batch=32, device=[1, 2])
