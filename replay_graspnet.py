919478d975ec420525e289af9bae3be382cf0427
Rui Cao <caorui0303@gmail.com>
2024-07-03 17:22:13 +0800

    update repo

--- replay_graspnet.py
+++ replay_graspnet.py
@@ -1,5 +1,5 @@
 import numpy as np
-method_id = 'GDS_v0.3.2'
+method_id = 'GDS_v0.2.1'
 dataset = 'Graspnet'
 camera = 'realsense'
 result = np.load('{}_{}_{}_results.npy'.format(dataset, method_id, camera))
@@ -9,6 +9,7 @@ result_dict['seen'] = result[:30]
 result_dict['similar'] = result[30:60]
 result_dict['novel'] = result[60:]
 
+print(dataset, method_id, camera)
 for split, result in result_dict.items():
     print('{} Prec:{:.4f}, Rec:{:.4f}, F_score:{:.4f}, Boundary Prec:{:.4f}, Rec:{:.4f}, F_score:{:.4f}, %75:{:.4f}'. \
         format(split, 
